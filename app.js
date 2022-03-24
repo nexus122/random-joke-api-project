78bfca52a2ef5de1abe9305b5f4a2d00b4f9f023
nexus122 <juan-pablo122@hotmail.es>
2022-03-24 14:07:41 +0100

    Chuck Norris Jokes Solution

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+const url = 'https://api.chucknorris.io/jokes/random';
+
+// Indicamos que la función es asincrona con async
+button.addEventListener('click', function(){    
+    // 1. Traernos el chiste de la api.
+    // 2. Parsear los datos que recivimos en formato json.
+    // 3. Actualizar el JokeDiv.textContent con la propiedad value del objeto que recivimos.
+    
+    peticion();
+    
+});
+
+async function peticion(){
+    // Creamos una llamada a la url de la api con fetch
+    let response = await fetch(url);
+    let chiste = await response.json();
+    jokeDIV.innerHTML = chiste.value
+}
+
+peticion();
+
+// Formas de hacer peticiones.
